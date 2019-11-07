f9d9e7aff682e258acd6dab64a659f3a4bc80da3
bbmattieu9 <bbmattieu9@gmail.com>
2019-11-07 09:49:02 +0100

    add onDelete func and onCheckOff func for the active to-do component

--- todoApp/src/app/active/active.component.ts
+++ todoApp/src/app/active/active.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-active',
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActiveComponent implements OnInit {
 
+  activeList = ['Pray', 'Read', 'Work'];
+  @Input() completedList;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onDelete(i) {
+    this.activeList.splice(i, 1);
+  }
+
+  onCheckOff(itemIndex) {
+    let removedItem = this.activeList.splice(itemIndex, 1);
+    this.completedList.push(removedItem);
   }
 
 }
